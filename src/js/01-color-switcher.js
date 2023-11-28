c625c1da8bd14ae1b72cd3033ca5e97a2eef0c63
WojtekHub <119422056+WojtekHub@users.noreply.github.com>
2023-11-28 14:59:10 +0100

    -

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,20 @@
+const results = document.querySelector('#results');
+const buttonStart = document.querySelector('#data-start');
+const buttonStop = document.querySelector('#data-stop');
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+buttonStart.addEventListener('click', () => {
+  const timer = setInterval(() => {
+    document.getElementById('results').style.backgroundColor =
+      getRandomHexColor();
+    //console.log(getRandomHexColor());
+  }, 2000);
+
+  buttonStop.addEventListener('click', () => {
+    clearInterval(timer);
+    console.log('TimeInterval STOP!');
+  });
+});
